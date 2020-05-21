018a01c96b3c00116ba713dbb8f8ea31ed5d05b7
MargaritaMulkeeva <mulkeevarita2001@gmail.com>
2020-05-21 18:18:28 +0600

    Задание2

---
+++ alchemy(1573)/alchemy(1573)/Alchemy.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace alchemy_1573_
+{
+    class Alchemy
+    {
+        private static int n;
+        private static uint[] c;
+
+        public static void InputData()
+        {
+            Console.WriteLine("Введите количество реагентов через пробел");
+            string b = Console.ReadLine();
+            string[] a = b.Split(new char[] { ' ' });
+            Console.WriteLine("Напишите количество цветов");
+            n = Convert.ToInt32(Console.ReadLine());
+            c = new uint[n];
+            if(n!=a.Length)
+            {
+                Console.WriteLine("Неодинаковое количество цветов");
+                return;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                c[i] = Convert.ToUInt32(a[i]);
+            }
+            Console.WriteLine("Введите названия " + n + " цветов через Enter");
+            for (int i = 0; i < n; i++)
+            {
+                Console.ReadLine();
+            }
+        }
+
+        public static void CountWays()
+        {
+            uint count=1;
+            for (int i = 0; i < n; i++)
+            {
+                count *= c[i];
+            }
+            Console.WriteLine("Количество способов: " + count);
+        }
+    }
+}
---
+++ alchemy(1573)/alchemy(1573)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace alchemy_1573_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alchemy.InputData();
+            Alchemy.CountWays();
+            Console.ReadKey();
+        }
+    }
+}
